d450021f20cfa25c9aac923bbdd236c5df5e74f6
zmartl-bbc <luca.marti@bluewin.ch>
2016-03-08 16:25:48 +0100

    Frontpage design
    Signed-off-by: zmartl-bbc <luca.marti@bluewin.ch>

---
+++ includes/head.php
@@ -0,0 +1,22 @@
+<meta charset="utf-8">
+
+<title>QuizMasters 2.0</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet"
+	href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+<!-- Latest compiled and minified jQuery -->
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+
+<!-- Latest compiled and minified JavaScript -->
+<script
+	src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+<!-- Custom CSS -->
+<link rel="stylesheet" href="style/custom.css">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
