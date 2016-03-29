be5d82c66915adb6edf082394f8f4476d5895042
Phong6698 <Phong@hotmail.ch>
2016-03-29 16:05:04 +0200

    merge

--- quiz.php
+++ quiz.php
@@ -11,62 +11,45 @@ include_once 'resources/login_registration.php';
 </head>
 <body>
 	<?php include_once 'resources/navigation.php'; ?>
-	<div id="myCarousel" class="carousel slide" data-ride="carousel">
-		<!-- Indicators -->
-		<ol class="carousel-indicators">
-			<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-			<li data-target="#myCarousel" data-slide-to="1"></li>
-		</ol>
-
-		<!-- Wrapper for slides -->
-		<div class="carousel-inner" role="listbox">
-			<div class="item active">
-				<img src="img/one.png" alt="" width="100%">
-			</div>
-			<div class="item">
-				<img src="img/two.png" alt="" width="100%">
+	<div class="container">
+		<div class="col-md-12">
+			<div class="row">
+				<img class="img-responsive img-width" alt="Test" src="img/title_small.png">
 			</div>
 		</div>
-
-		<!-- Left and right controls -->
-		<a class="left carousel-control" href="#myCarousel" role="button"
-			data-slide="prev"> <span class="glyphicon glyphicon-chevron-left"
-			aria-hidden="true"></span> <span class="sr-only">Previous</span>
-		</a> <a class="right carousel-control" href="#myCarousel"
-			role="button" data-slide="next"> <span
-			class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-			<span class="sr-only">Next</span>
-		</a>
-	</div>
-	<div class="container">
 		<div class="row">
-			<?php
-
-			$quiz = Database::getInstance ()->createQuiz ( $_GET ['id'], $categories );
-			foreach ( $quiz->__get ( 'questions' ) as $question ) {
-				?>
-			<div class="col-md-12">
-				<div class="panel panel-default">
-					<div class="panel-heading">
-						<h3 class="panel-title">
-							<?php echo utf8_encode($question->__get('question'))?>
-						</h3>
-					</div>
-					<div class="panel-body">
-						<div class="row">
-							<?php foreach ($question->__get('answers') as $answer){?>
-
-							<div class="col-md-6">
-								<input type="radio" id="" name="" value="">
-								<?php echo utf8_encode($answer->__get('answer'))?>
+			<form action="result.php" method="post">
+				<?php
+	
+				$quiz = Database::getInstance ()->createQuiz ( $_GET ['id'], $categories );
+				$_SESSION['quiz'] = serialize($quiz);
+				foreach ( $quiz->__get ( 'questions' ) as $question ) {
+					?>
+				<div class="col-md-12">
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h3 class="panel-title">
+								<?php echo utf8_encode($question->__get('question'))?>
+							</h3>
+						</div>
+						<div class="panel-body">
+							<div class="row">
+								<?php foreach ($question->__get('answers') as $answer){?>
+	
+								<div class="col-md-6">
+									<input type="radio" id="<?php echo $answer->__get('answerID')?>" name="<?php echo $question->__get('questionID')?>" value="<?php echo $answer->__get('answerID')?>">															
+										<label for="<?php echo $answer->__get('answerID')?>"><span><span></span></span><?php echo utf8_encode($answer->__get('answer'))?></label>
+								</div>
+								
+	
+								<?php } ?>
 							</div>
-
-							<?php } ?>
 						</div>
 					</div>
 				</div>
-			</div>
-			<?php }?>
+				<?php }?>
+				<input type="submit" class="button" value="Auswertung">
+			</form>
 		</div>
 	</div>
 
