2535a5d3c1a9881267038df96f53da9dc43fe4f1
Phong6698 <Phong@hotmail.ch>
2016-03-29 13:47:29 +0200

    merge

--- database/database_infos.php
+++ database/database_infos.php
@@ -2,7 +2,7 @@
 require_once 'Database.php';
 
 //Needed Information for connection
-$server_name = "WIN-4GVDTODFV60\SQLSERVER";
+$server_name = "WIN-6I7SLMJ1GDI\SQLSERVER";
 $db_infos = array( "Database"=>"QUIZMASTERS");
 
 //checks if connection is set otherwise it creates a new connection
