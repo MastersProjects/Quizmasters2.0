373e9f1a68cdd31015ba34062a0afa14830e5b9a
Phong6698 <Phong@hotmail.ch>
2016-04-03 16:12:09 +0200

    db info

--- database/database_infos.php
+++ database/database_infos.php
@@ -2,7 +2,11 @@
 require_once 'Database.php';
 
 //Needed Information for connection
-$server_name = "WIN-4GVDTODFV60\SQLSERVER";
+//For VM
+$server_name = "WIN-6I7SLMJ1GDI\SQLSERVER";
+// //For Elia
+// $server_name = "WIN-4GVDTODFV60\SQLSERVER";
+
 $db_infos = array( "Database"=>"QUIZMASTERS");
 
 //checks if connection is set otherwise it creates a new connection
